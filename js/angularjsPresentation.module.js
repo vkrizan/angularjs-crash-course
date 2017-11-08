a115b27a05ad030d57e9410cb2c7020103d4df93
Viliam Krizan <vkrizan@redhat.com>
2017-11-08 17:40:26 +0100

    show slide number in footer

--- js/angularjsPresentation.module.js
+++ js/angularjsPresentation.module.js
@@ -11,6 +11,12 @@ angular.module('angularjsPresentation', [
             });
 
     }
-).controller('SlidesController', ['$rootScope', function($rootScope) {
+).controller('SlidesController', ['$rootScope', '$scope', function($rootScope, $scope) {
     $rootScope.total_slides = 10;
+
+    $scope.$on('$routeChangeSuccess', function($event, next, current) {
+        if (next) {
+            $scope.slide_num = next.$$route.originalPath.substr(1) || '1';
+        }
+    });
 }]);
